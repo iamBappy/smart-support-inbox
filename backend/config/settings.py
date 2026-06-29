900fcbf82957ab74aae69a9a68869a2583102195
Bappy <sharif.mizarul@gmail.com>
2026-06-30 01:52:48 +0600

    JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)(a3x0b*x!v-%m-e)81f(4=kwfatms8@wjwjfq#uxs%u*!d9o'
+SECRET_KEY = "django-insecure-smart-support-inbox-secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -144,3 +145,5 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
     )
 }
+
+
